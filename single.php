1a25750aede582e27799cf85f8caf66d479e6756
Trey Piepmeier <trey@treypiepmeier.com>
2015-10-04 23:53:32 -0400

    Post navigation on permalink pages.
    Easily browse to the previous and next photos.

--- single.php
+++ single.php
@@ -13,11 +13,16 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 		<?php while ( have_posts() ) : the_post(); ?>
-
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
 		<?php endwhile; ?>
 
+			<nav class="navigation post-navigation" role="navigation">
+				<h2 class="screen-reader-text"><?php esc_html_e( 'Post navigation', 'artisanalvarmint' ); ?></h2>
+				<div class="nav-links">
+					<div class="nav-previous"><?php previous_post_link('%link'); ?></div>
+					<div class="nav-next"><?php next_post_link('%link'); ?></div>
+				</div>
+			</nav>
 		</main>
 	</div>
 
